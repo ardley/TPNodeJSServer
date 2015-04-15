219cb390c4d47975eb5955a9d9ed4efdabad40cf
Sean Ardley <sean.ardley@gmail.com>
2015-04-15 19:31:46 +0000

    added all files

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * Module dependencies.
+ */
+var express  = require('express');
+var connect = require('connect');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var toobusy = require('toobusy');
+
+
+// Configuration
+app.use(function(requ, res, next){
+        if (toobusy()) res.send(503, "I'm busy right now, sorry (DDOS attempt likely in progress)");
+        else next();
+        });
+
+app.use(express.static(__dirname + '/public'));
+app.use(connect.logger('dev'));
+app.use(connect.json());
+app.use(connect.urlencoded());
+
+// Routes
+require('./routes/routes.js')(app);
+app.listen(port);
+console.log('The App runs on port ' + port);
+
+
---
+++ routes/routes.js
@@ -0,0 +1,39 @@
+var testing = require('config/testing');
+var onboarding = require('config/onboarding');
+var toobusy = require('toobusy');
+
+module.exports = function(app) {
+  app.use(function(req, res, next){
+        if(toobusy())res.send(503, "I'm tooo busy, maybe a ddos attempt");
+        else next();
+        });
+  app.get('/', function(req, res) {
+    res.end("Welcome to ClipHorde.  Please see the kickstarter to help make it go.");
+  });
+
+  app.post('/api/testing', function(req, res) {
+    console.log(req.body);
+    var deviceTypeKey = req.body.deviceType;
+    console.log(deviceTypeKey);
+    var installIDKey = req.body.InstallID;
+    console.log(installIDKey);
+    var installRHKey = req.body.InstallRandHex;
+    console.log(installRHKey);
+   
+    testing.init_regist(deviceTypeKey,installIDKey,installRHKey, function(found){
+      console.log(found);
+      res.json(found);
+    });
+   });
+
+  app.post('/api/onboarding', function(req, res) {
+    var deviceTypeKey = req.body.deviceType;
+    var installIDKey = req.body.InstallID;
+    onboarding.init_regist(deviceTypeKey,installIDKey,function(found){
+      console.log(found);
+      res.json(found);
+  });
+  });
+
+}
+
